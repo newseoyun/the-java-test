e2700b8519ef7291a9adcc2f2d321ef591c0796e
macSeoyun <newseoyun@gmail.com>
2021-12-20 00:12:26 +0900

    .

--- src/test/java/hi/thejavatest/StudyTest.java
+++ src/test/java/hi/thejavatest/StudyTest.java
@@ -2,6 +2,19 @@ package hi.thejavatest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
 import java.util.function.Supplier;
@@ -51,6 +64,27 @@ class StudyTest {
                 // assertThat 은 assertj 라이브러리가 제공하는 메소드다.
                 () -> assertThat(study.getLimit()).isGreaterThan(0)
         );
+    }
+
+    @BeforeAll // static 으로만. return 타입 없어야.
+    static void beforeAll() {
+        System.out.println("before All");
+    }
+
+    @AfterAll // static 으로만. return 타입 없어야.
+    static void afterAll() {
+        System.out.println("after All");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before Each");
+    }
+
+    @Test
+    @Disabled
+    void disabled_test() {
+        System.out.println("disabled_test");
     }
 
     @Test
@@ -104,18 +138,18 @@ class StudyTest {
     }
 
     @Test
-    @DisplayName("Enabled 애노테이션 테스트")
-    @EnabledOnOs(OS.MAC)
-    @EnabledOnJre({JRE.JAVA_8, JRE.JAVA_11})
-    void enabled_annotation() {
+    @DisplayName("Enabled If ~ 애노테이션 테스트")
+    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "LOCAL")
+    void enabled_if_annotation() {
         Study study = new Study(100);
         assertThat(study.getLimit()).isGreaterThan(0);
     }
 
     @Test
-    @DisplayName("Enabled If ~ 애노테이션 테스트")
-    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "LOCAL")
-    void enabled_if_annotation() {
+    @DisplayName("Enabled 애노테이션 테스트")
+    @EnabledOnOs(OS.MAC)
+    @EnabledOnJre({JRE.JAVA_8, JRE.JAVA_11})
+    void enabled_annotation() {
         Study study = new Study(100);
         assertThat(study.getLimit()).isGreaterThan(0);
     }
@@ -130,29 +164,78 @@ class StudyTest {
     }
 
     @Test
-    void create_new_study() {
-        System.out.println("create2");
+    @DisplayName("특정 태그를 붙인 테스트만 테스트하거나 제외해서 테스트할 수 있다.")
+    @Tag("slow") //  IDE의 Edit configurations에서 설정하거나 pom.xml or build.gradle 에서도 설정 가능.
+    void tagging() {
+        System.out.println("tagging");
     }
 
     @Test
-    @Disabled
-    void create_disabled() {
-        System.out.println("create1");
+    @DisplayName("커스텀 컴포즈 애노테이션")
+    @FastTest // 커스텀 애노테이션. @Test와 @Tag를 조합한, 컴포즈 애노테이션이다.
+    void custom_annotation() {
+        System.out.println("custom_annotation");
     }
 
-    @BeforeAll // static 으로만. return 타입 없어야.
-    static void beforeAll() {
-        System.out.println("before All");
+    @DisplayName("테스트 반복")
+    @RepeatedTest(10) // (value = 10, name = "{displayName}, {currentRepetition}/{totalRepetition}")
+    void repeat_test(RepetitionInfo repetitionInfo) {
+        System.out.println("TEST " + repetitionInfo.getCurrentRepetition() + "/" +
+                repetitionInfo.getTotalRepetitions());
     }
 
-    @AfterAll // static 으로만. return 타입 없어야.
-    static void afterAll() {
-        System.out.println("after All");
+    @ParameterizedTest // (name = "{index} {displayName} message={0}")
+    @DisplayName("파라미터를 줄 수 있는 테스트")
+    @ValueSource(strings = {"하이", "반갑습니다", "안녕하세요"})
+    @EmptySource
+    @NullSource
+    void ParameterizedTest(String str) {
+        System.out.println("str: " + str);
     }
 
-    @BeforeEach
-    void beforeEach() {
-        System.out.println("Before Each");
+    @ParameterizedTest // (name = "{index} {displayName} message={0}")
+    @DisplayName("객체에 파라미터 넘기기")
+    @ValueSource(ints = {10, 20, 40})
+    void ParameterizedTest2(@ConvertWith(StudyConverter.class) Study study) {
+        System.out.println(study.getLimit());
+    }
+
+    static class StudyConverter extends SimpleArgumentConverter {
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType, "Study 타입만 Convert 가능.");
+            return new Study(Integer.parseInt(source.toString()));
+        }
+    }
+
+    @ParameterizedTest // (name = "{index} {displayName} message={0}")
+    @DisplayName("객체에 CSV 형태 파라미터 넘기기")
+    @CsvSource({"10, '자바 스터디'", "20, '스프링 스터디'"})
+    void ParameterizedTest3(Integer limit, String name) {
+        Study study = new Study(limit, name);
+        System.out.println(study.toString());
+    }
+
+    @ParameterizedTest // (name = "{index} {displayName} message={0}")
+    @DisplayName("객체에 CSV 형태 파라미터 넘기기 ArgumentsAccessor")
+    @CsvSource({"10, '자바 스터디'", "20, '스프링 스터디'"})
+    void ParameterizedTest4(ArgumentsAccessor argumentsAccessor) {
+        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        System.out.println(study);
+    }
+
+    @ParameterizedTest // (name = "{index} {displayName} message={0}")
+    @DisplayName("객체에 CSV 형태 파라미터 넘기기 Custom ArgumentsAggregator")
+    @CsvSource({"10, '자바 스터디'", "20, '스프링 스터디'"})
+    void ParameterizedTest5(@AggregateWith(StudyAggregator.class) Study study) {
+        System.out.println(study);
+    }
+
+    static class StudyAggregator implements ArgumentsAggregator {
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context) throws ArgumentsAggregationException {
+            return new Study(accessor.getInteger(0), accessor.getString(1));
+        }
     }
 
 }
